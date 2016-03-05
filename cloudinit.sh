d1af8d2ff7b8be4bd01f32e8e61b6916663d8042
AJ Bowen <aj@gandi.net>
2016-03-05 01:16:45 +0100

    move the rest of cloudinit things to postprep.rc

--- cloudinit.sh
+++ cloudinit.sh
@@ -3,39 +3,37 @@
 
 # On EC2, the ephemeral disk might be mounted on /mnt.
 # If /mnt is a mountpoint, place Docker workspace on it.
-if mountpoint -q /mnt
-then
-	mkdir /mnt/docker
-	ln -s /mnt/docker /var/lib/docker
-fi
+#if mountpoint -q /mnt
+#then
+#	mkdir /mnt/docker
+#	ln -s /mnt/docker /var/lib/docker
+#fi
 
 # Set the hostname to be the public IP address of the instance.
 # If the call to myip fails, set a default hostname.
-if ! curl --silent --fail http://myip.enix.org/REMOTE_ADDR >/etc/hostname; then
-    echo dockerhost >/etc/hostname
-fi
-hostname $(cat /etc/hostname)
+#if ! curl --silent --fail http://myip.enix.org/REMOTE_ADDR >/etc/hostname; then
+#    echo dockerhost >/etc/hostname
+#fi
+#hostname $(cat /etc/hostname)
 
 # Temporary prompt to show that we're not done cloudinit-ing yet.
 #update_status " [creating docker user]"
 
 # Create Docker user.
-useradd -d /home/docker -m -s /bin/bash docker
+#useradd -d /home/docker -m -s /bin/bash docker
 
-echo docker:training | chpasswd
+#echo docker:training | chpasswd
 
-echo "docker ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/docker
+#echo "docker ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/docker
 
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-service ssh restart
+#sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+#service ssh restart
 
-#update_status " [creating docker user]"
-apt-get -q update
-apt-get -qy install git jq python-pip
+#apt-get -q update
+#apt-get -qy install git jq python-pip
 
 # This will install the latest Docker.
-#update_status " [installing docker]"
-curl https://get.docker.com/ | sh
+#curl https://get.docker.com/ | sh
 
 # Make sure that the daemon listens on 55555 (for orchestration workshop).
 #sed -i 's,-H fd://$,-H fd:// -H tcp://0.0.0.0:55555,' /lib/systemd/system/docker.service
@@ -53,10 +51,10 @@ curl https://get.docker.com/ | sh
 
 # Wait for docker to be up.
 # If we don't do this, Docker will not be responsive during the next step.
-while ! docker version
-do
-	sleep 1
-done
+#while ! docker version
+#do
+#	sleep 1
+#done
 
 # Pre-pull a bunch of images.
 #for I in \
